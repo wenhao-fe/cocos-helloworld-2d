0695bb59b74d7da8e9fb27c2155d6c0d684dc8ff
wenhao <15101516312@163.com>
2023-05-30 16:15:52 +0800

    init

---
+++ assets/Script/PlayerController.ts
@@ -0,0 +1,14 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('PlayerComponent')
+export class PlayerComponent extends Component {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+
+    }
+}
+
